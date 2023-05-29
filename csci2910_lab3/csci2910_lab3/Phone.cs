2b26a06b16a34d22cf63edc83d4a71f9ff93c2e7
TMAWilliams <tmawilliams73@gmail.com>
2023-05-29 14:22:20 -0400

    Set up inital file structure for program.

---
+++ csci2910_lab3/csci2910_lab3/Phone.cs
@@ -0,0 +1,22 @@
+﻿/**       
+ *--------------------------------------------------------------------
+ * 	   File name: Phone.cs
+ * 	Project name: csci2910_lab3
+ *--------------------------------------------------------------------
+ * Author’s name and email:	 Tessa Williams williamstm5@etsu.edu				
+ *          Course-Section: CSCI-2910-970
+ *           Creation Date:	 05/29/2023		
+ * -------------------------------------------------------------------
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csci2910_lab3
+{
+    public class Phone
+    {
+    }
+}
---
+++ csci2910_lab3/csci2910_lab3/RandomDataGenerator.cs
@@ -0,0 +1,21 @@
+﻿/**       
+ *--------------------------------------------------------------------
+ * 	   File name: RandomDataGenerator.cs
+ * 	Project name: csci2910_lab3
+ *--------------------------------------------------------------------
+ * Author’s name and email:	 Tessa Williams williamstm5@etsu.edu				
+ *          Course-Section: CSCI-2910-970
+ *           Creation Date:	 05/29/2023		
+ * -------------------------------------------------------------------
+ */
+
+namespace csci2910_lab3
+{
+    public class RandomDataGenerator
+    {
+        static void Main()
+        {
+            
+        }
+    }
+}
