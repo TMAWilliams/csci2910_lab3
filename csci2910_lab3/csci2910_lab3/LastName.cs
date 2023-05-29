356e4e72e53d2360bc1e718d99ab7b2d12c821c5
TMAWilliams <tmawilliams73@gmail.com>
2023-05-29 14:37:58 -0400

    Filled enum LastName with possible last names from UML.

--- csci2910_lab3/csci2910_lab3/LastName.cs
+++ csci2910_lab3/csci2910_lab3/LastName.cs
@@ -18,5 +18,25 @@ namespace csci2910_lab3
 {
     public enum LastName
     {
+        Smith,
+        Johnson,
+        Williams,
+        Brown,
+        Jones,
+        Miller,
+        Davis,
+        Garcia,
+        Rodriguez,
+        Wilson,
+        Martinez,
+        Anderson,
+        Taylor,
+        Thomas,
+        Hernandez,
+        Moore,
+        Martin,
+        Jackson,
+        Thompson,
+        White
     }
 }
