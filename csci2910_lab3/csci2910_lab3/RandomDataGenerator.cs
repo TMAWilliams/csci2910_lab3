2447acf4431cd9f85bbaa0ad19dac69bf1234c4b
TMAWilliams <tmawilliams73@gmail.com>
2023-05-30 13:40:21 -0400

    Implemented code in the main driver for the following methods: DisplayMenu, AddPerson, RemovePerson.

--- csci2910_lab3/csci2910_lab3/RandomDataGenerator.cs
+++ csci2910_lab3/csci2910_lab3/RandomDataGenerator.cs
@@ -17,5 +17,46 @@ namespace csci2910_lab3
         {
             
         }
+        /// <summary>
+        /// Displays main menu
+        /// </summary>
+        static void DisplayMenu()
+        {
+            Console.WriteLine("-----Main Menu-----");
+            Console.WriteLine("1.Generate Random Person");
+            Console.WriteLine("2.View All Persons");
+            Console.WriteLine("3.Remove Person");
+            Console.WriteLine("4.Generate Random Last Name");
+            Console.WriteLine("5.Generate Random SSN");
+            Console.WriteLine("6.Generate Random Phone Number");
+            Console.WriteLine("0.Exit Program");
+        }
+        /// <summary>
+        /// Adds a person to a list of people
+        /// </summary>
+        /// <param name="person">Person - person to add</param>
+        /// <param name="people">List<Person> - list of people to add too</param>
+        static void AddPerson(Person person, List<Person> people)
+        {
+            people.Add(person);
+        }
+        /// <summary>
+        /// Removes a person from a list if they exist in that list else display an error message.
+        /// </summary>
+        /// <param name="person">Person - person to remove</param>
+        /// <param name="people">List<Person> - list of people to remove from</param>
+        static void RemovePerson(Person person, List<Person> people)
+        {
+            if (people.Contains(person))
+            {
+                people.Remove(person);
+                Console.WriteLine($"{person.FirstName} {person.LastName} was removed.");
+            }else
+            {
+                Console.WriteLine($"Error {person.FirstName} {person.LastName} was not found.");
+            }
+        }
+
+
     }
 }
